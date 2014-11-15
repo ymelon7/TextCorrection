571087e066973b881ab83636f3b092af9a98cdc1
ymelon7 <ymelon7@gmail.com>
2014-11-14 21:36:27 -0800

    统计查询时间

--- src/test_main.cpp
+++ src/test_main.cpp
@@ -8,12 +8,13 @@
 #include "TextQuery.h"
 #include <iostream>
 #include <muduo/base/Logging.h>
+#include <muduo/base/Timestamp.h>
 
 using namespace muduo;
 
 int main (int argc, char *argv[])
 {
-    Logger::setLogLevel(Logger::DEBUG); //设置日志打印级别
+   // Logger::setLogLevel(Logger::DEBUG); //设置日志打印级别
 
     TextQuery query("../dict/en.dict", 
                     "../dict/ch.dict",
@@ -22,6 +23,12 @@ int main (int argc, char *argv[])
     std::string word;
     while(std::cin >> word)
     {
+        Timestamp t1(Timestamp::now());
+
         std::cout << query.queryWord(word) << std::endl;  
+
+        Timestamp t2(Timestamp::now());
+
+        std::cout << "query cost time: "<< timeDifference(t2, t1) << " s" << std::endl;
     }
 }
